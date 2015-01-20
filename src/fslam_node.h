68bd00d1a052db8893d5d5732cc6a6a4a9a21e2b
Martin Llofriu <mllofriu@gmail.com>
2015-01-19 19:26:49 -0500

    Increase rotation noise, decrease particles to 100

--- src/fslam_node.h
+++ src/fslam_node.h
@@ -17,7 +17,7 @@
 #define MU_SYSTEM_NOISE_THETA 0.0
 #define SIGMA_SYSTEM_NOISE_X 0.2
 #define SIGMA_SYSTEM_NOISE_Y 0.2
-#define SIGMA_SYSTEM_NOISE_THETA 0.03
+#define SIGMA_SYSTEM_NOISE_THETA 0.05
 
 #define MU_MEAS_NOISE_X 0.0
 #define MU_MEAS_NOISE_Y 0.0
@@ -26,7 +26,7 @@
 #define SIGMA_MEAS_NOISE_Y .2
 #define SIGMA_MEAS_NOISE_Z .2
 
-#define NUM_SAMPLES 200 // Default Number of Samples
+#define NUM_SAMPLES 100 // Default Number of Samples
 #define RESAMPLE_PERIOD 0 // Default Resample Period
 #define RESAMPLE_THRESHOLD (NUM_SAMPLES/4.0) // Threshold for Dynamic Resampling
 
