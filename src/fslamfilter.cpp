8a0918a3371bfe671aec34616f95b87990fd304b
Martin Llofriu <mllofriu@gmail.com>
2015-01-20 13:08:10 -0500

    Added landmark marker publishing

--- src/fslamfilter.cpp
+++ src/fslamfilter.cpp
@@ -2,16 +2,22 @@
 
 #include <filter/bootstrapfilter.h>
 
+#include <visualization_msgs/Marker.h>
+
 using namespace robot_pose_fslam;
 using namespace BFL;
 using namespace std;
 using namespace tf;
+using namespace ros;
 
 FSLAMFilter::FSLAMFilter(MCPdf<vector<TransformWithCovarianceStamped> > * prior,
 		int resample_period, double resample_thrs) :
 		BootstrapFilter<vector<TransformWithCovarianceStamped>,
 				TransformWithCovarianceStamped>(prior, resample_period,
 				resample_thrs) {
+
+	rvizMarkerPub_ = NodeHandle().advertise<visualization_msgs::Marker>(
+			"visualization_marker", 0);
 }
 
 void FSLAMFilter::mapping(const TransformWithCovarianceStamped & m) {
@@ -52,7 +58,7 @@ void FSLAMFilter::mapping(const TransformWithCovarianceStamped & m) {
 			transformMsgToTF(stateIter->transform.transform, stateT);
 			tf::Transform measurementT;
 			transformMsgToTF(measurement.transform.transform, measurementT);
-			measurementT.mult(stateT,measurementT);
+			measurementT.mult(stateT, measurementT);
 			transformTFToMsg(measurementT, measurement.transform.transform);
 			measurement.header.frame_id = stateIter->header.frame_id;
 			// TODO: Covariance
@@ -65,8 +71,9 @@ void FSLAMFilter::mapping(const TransformWithCovarianceStamped & m) {
 
 }
 
-void FSLAMFilter::publishTF(tf::TransformBroadcaster & br, std::string & robotFrame, bool publishLandmarks) {
-  tf::Transform robotT;
+void FSLAMFilter::publishTF(tf::TransformBroadcaster & br,
+		std::string & robotFrame, bool publishLandmarks) {
+	tf::Transform robotT;
 
 	MCPdf<vector<TransformWithCovarianceStamped> > * mcpdf = PostGet();
 	for (int i = 0; i < mcpdf->NumSamplesGet(); i++) {
@@ -87,44 +94,97 @@ void FSLAMFilter::publishTF(tf::TransformBroadcaster & br, std::string & robotFr
 
 		tf::Transform t;
 		transformMsgToTF(stateIter->transform.transform, t);
-    
+
 //		ROS_INFO("Particle x %f", t.getOrigin().getX());
 		char child_frame[10];
 		ros::Time partTime = stateIter->header.stamp;
 		sprintf(&child_frame[0], "part%d", i);
-		tf::StampedTransform st(t, partTime,
-				stateIter->header.frame_id, child_frame);
+		tf::StampedTransform st(t, partTime, stateIter->header.frame_id,
+				child_frame);
 		br.sendTransform(st);
 
-    // Acumulate transform for the robot pos
-    t.setOrigin(t.getOrigin() / mcpdf->NumSamplesGet());
-    t.setRotation(t.getRotation());
-    robotT.setOrigin(robotT.getOrigin() + t.getOrigin());
-    robotT.setRotation(robotT.getRotation() + t.getRotation());    
-    //robotT.mult(robotT, t);
+		// Acumulate transform for the robot pos
+		t.setOrigin(t.getOrigin() / mcpdf->NumSamplesGet());
+		t.setRotation(t.getRotation());
+		robotT.setOrigin(robotT.getOrigin() + t.getOrigin());
+		robotT.setRotation(robotT.getRotation() + t.getRotation());
+		//robotT.mult(robotT, t);
 
 		// Publish landmarks
-    	if (publishLandmarks){
+		if (publishLandmarks) {
 			int j = 0;
-			for (stateIter = state.begin(); stateIter != state.end(); stateIter++) {
+			for (stateIter = state.begin(); stateIter != state.end();
+					stateIter++) {
 				if (stateIter->child_frame_id.compare(robotFrame) != 0) {
+					// Send the transform
 					tf::Transform t;
 					transformMsgToTF(stateIter->transform.transform, t);
 					//		ROS_INFO("Particle x %f", t.getOrigin().getX());
 					char marker_frame[15];
-					sprintf(&marker_frame[0], "part%d%s", i, stateIter->child_frame_id.c_str());
+					sprintf(&marker_frame[0], "part%d%s", i,
+							stateIter->child_frame_id.c_str());
 					tf::StampedTransform st(t, partTime,
 							stateIter->header.frame_id, marker_frame);
 					br.sendTransform(st);
+					// Send the landmark
+					publishVisualMarker(st, stateIter->header.frame_id,
+							partTime, i * 4 + j, stateIter->child_frame_id);
 					j++;
 				}
 			}
-    	}
+		}
 
 	}
 
-  
-  br.sendTransform(tf::StampedTransform(robotT, ros::Time::now(), "map", "robot"));
-  ROS_DEBUG("TF particles published");
+	br.sendTransform(
+			tf::StampedTransform(robotT, ros::Time::now(), "map", "robot"));
+	ROS_DEBUG("TF particles published");
 }
 
+void FSLAMFilter::publishVisualMarker(tf::StampedTransform pose,
+		string frame_id, Time stamp, int markerId, string mId) {
+	static int id = 0;
+
+	visualization_msgs::Marker rvizMarker_;
+	tf::poseTFToMsg(pose, rvizMarker_.pose);
+
+	rvizMarker_.header.frame_id = frame_id;
+	rvizMarker_.header.stamp = stamp;
+	rvizMarker_.id = id++;
+
+	rvizMarker_.scale.x = 1.0;
+	rvizMarker_.scale.y = 1.0;
+	rvizMarker_.scale.z = 0.5;
+	rvizMarker_.ns = "basic_shapes";
+	rvizMarker_.type = visualization_msgs::Marker::CUBE;
+	rvizMarker_.action = visualization_msgs::Marker::ADD;
+	if (mId.compare("/M1") == 0) {
+		rvizMarker_.color.r = 0.0f;
+		rvizMarker_.color.g = 0.0f;
+		rvizMarker_.color.b = 1.0f;
+		rvizMarker_.color.a = 1.0;
+	} else if (mId.compare("/M2") == 0) {
+		rvizMarker_.color.r = 1.0f;
+		rvizMarker_.color.g = 0.0f;
+		rvizMarker_.color.b = 0.0f;
+		rvizMarker_.color.a = 1.0;
+	} else if (mId.compare("/M3") == 0) {
+		rvizMarker_.color.r = 1.0f;
+		rvizMarker_.color.g = 0.0f;
+		rvizMarker_.color.b = 1.0f;
+		rvizMarker_.color.a = 1.0;
+	} else if (mId.compare("/M4") == 0) {
+		rvizMarker_.color.r = 1.0f;
+		rvizMarker_.color.g = 1.0f;
+		rvizMarker_.color.b = 0.0f;
+		rvizMarker_.color.a = 1.0;
+	} else {
+		rvizMarker_.color.r = 0.0f;
+		rvizMarker_.color.g = 1.0f;
+		rvizMarker_.color.b = 0.0f;
+		rvizMarker_.color.a = 1.0;
+	}
+	rvizMarker_.lifetime = ros::Duration(30);
+
+	rvizMarkerPub_.publish(rvizMarker_);
+}
--- src/fslamfilter.h
+++ src/fslamfilter.h
@@ -14,16 +14,24 @@
 #include <robot_pose_fslam/TransformWithCovarianceStamped.h>
 #include <tf/transform_broadcaster.h>
 
-class FSLAMFilter : public BFL::BootstrapFilter<std::vector<robot_pose_fslam::TransformWithCovarianceStamped>,
-robot_pose_fslam::TransformWithCovarianceStamped>
-{
+class FSLAMFilter: public BFL::BootstrapFilter<
+		std::vector<robot_pose_fslam::TransformWithCovarianceStamped>,
+		robot_pose_fslam::TransformWithCovarianceStamped> {
 public:
-	FSLAMFilter(BFL::MCPdf<std::vector<robot_pose_fslam::TransformWithCovarianceStamped> > * prior, int resample_period, double resample_thrs);
-	void mapping(const robot_pose_fslam::TransformWithCovarianceStamped & measurement);
-	void publishTF(tf::TransformBroadcaster &, std::string &, bool publishLandmarks);
+	FSLAMFilter(
+			BFL::MCPdf<
+					std::vector<robot_pose_fslam::TransformWithCovarianceStamped> > * prior,
+			int resample_period, double resample_thrs);
+	void mapping(
+			const robot_pose_fslam::TransformWithCovarianceStamped & measurement);
+	void publishTF(tf::TransformBroadcaster &, std::string &,
+			bool publishLandmarks);
 
+private:
+	void publishVisualMarker(tf::StampedTransform pose, std::string frame_id,
+			ros::Time stamp, int markerId, std::string mId);
+
+	ros::Publisher rvizMarkerPub_;
 };
-
-
 
 #endif /* FSLAMFILTER_H_ */
